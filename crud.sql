a825b4880aabd1a5c161a8fb8b2e34024a6f070e
orelhudo0369 <136131319+orelhudo0369@users.noreply.github.com>
2023-09-29 11:15:21 -0300

    Add files via upload

--- crud.sql
+++ crud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 28-Set-2023 às 15:59
+-- Tempo de geração: 29-Set-2023 às 14:18
 -- Versão do servidor: 8.0.31
--- versão do PHP: 8.0.26
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `noticias`
+--
+
+DROP TABLE IF EXISTS `noticias`;
+CREATE TABLE IF NOT EXISTS `noticias` (
+  `id_noticia` int NOT NULL AUTO_INCREMENT,
+  `titulo_noticia` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `texto_noticia` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_noticia`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `skin`
 --
 
 DROP TABLE IF EXISTS `skin`;
 CREATE TABLE IF NOT EXISTS `skin` (
   `id_skin` int NOT NULL AUTO_INCREMENT,
-  `nome_skin` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `nome_skin` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `preco_skin` decimal(10,2) NOT NULL,
-  `imagem_skin` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `float_skin` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `imagem_skin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `float_skin` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_skin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `skin`
+--
+
+INSERT INTO `skin` (`id_skin`, `nome_skin`, `preco_skin`, `imagem_skin`, `float_skin`) VALUES
+(1, 'Green Doppler', '4400.00', 'images.jpg', '0.330001');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
