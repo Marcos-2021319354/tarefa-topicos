c61964334c218d9b5fa0ced7916c0716067dec5e
orelhudo0369 <136131319+orelhudo0369@users.noreply.github.com>
2023-09-29 11:05:49 -0300

    Add files via upload

--- editar_skin.php
+++ editar_skin.php
@@ -5,12 +5,11 @@ include_once 'conecta.php';
 $id_skin = $_GET['id_skin'];
 $nome_skin = $_POST['nome_skin'];
 $preco_skin = $_POST['preco_skin'];
-
 $float_skin = $_POST['float_skin'];
-
+//Imagem
 $imagem_skin = $_FILES['imagem_skin']['name'];
 $imagem_skin_tmp = $_FILES['imagem_skin']['tmp_name'];
-
+if (isset($_FILES['imagem_skin']['name'][0]) and !empty($_FILES['imagem_skin']['name'][0])) {
 
 
 $sql = "UPDATE `skin` SET `nome_skin` = '$nome_skin', `preco_skin` = '$preco_skin', `imagem_skin` = '$imagem_skin', `float_skin` = '$float_skin' 
@@ -28,6 +27,22 @@ if ($resultado)
 } else {
 	echo "Erro 421";
 }
+} else {
+	$sql = "UPDATE `skin` SET `nome_skin` = '$nome_skin', `preco_skin` = '$preco_skin', `float_skin` = '$float_skin' 
+WHERE `id_skin` = ".$id_skin;
 
+
+$resultado = $conn -> query($sql);
+
+
+
+if ($resultado)
+
+{
+	header('location: index.php');
+} else {
+	echo "Erro 421";
+}
+}
 
 ?>
